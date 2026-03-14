3dccef266533700cb7e82b60053a6cb9bc5e5da3
nitinpcs <nitinpachauri937@gmail.com>
2026-03-14 23:29:26 +0530

    Time: 59 ms (71.52%) | Memory: 125.9 MB (88.94%) - LeetSync

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -0,0 +1,47 @@
+class Solution {
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+    public int minimumObstacles(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] dist = new int[n][m];
+
+        for(int[] row : dist)
+            Arrays.fill(row, Integer.MAX_VALUE);
+
+        Deque<int[]> dq = new ArrayDeque<>();
+
+        dq.offerFirst(new int[]{0,0});
+        dist[0][0] = 0;
+
+        while(!dq.isEmpty()){
+
+            int[] cur = dq.pollFirst();
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int i=0;i<4;i++){
+
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+
+                if(nx<0 || ny<0 || nx>=n || ny>=m)
+                    continue;
+
+                int cost = grid[x][y];
+
+                if(dist[x][y] + cost < dist[nx][ny]){
+
+                    dist[nx][ny] = dist[x][y] + cost;
+
+                    if(cost == 0)
+                        dq.offerFirst(new int[]{nx,ny});
+                    else
+                        dq.offerLast(new int[]{nx,ny});
+                }
+            }
+        }
+
+        return dist[n-1][m-1];
+    }
+}
