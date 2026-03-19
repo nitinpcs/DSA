dcd9d437812f8a20d7cad031e6ae333bac1b059e
nitinpcs <nitinpachauri937@gmail.com>
2026-03-19 14:26:46 +0530

    Time: 3 ms (100.00%) | Memory: 103.5 MB (58.85%) - LeetSync

---
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        int n = edges.length;
+        long[] score = new long[n];
+        for(int i=0; i<n; i++){
+            score[edges[i]] += i;
+        }
+        long maxS = Long.MIN_VALUE;
+        int idx = 0;
+        for(int i=0; i<n; i++){
+            if(score[i] > maxS){
+                maxS = score[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
