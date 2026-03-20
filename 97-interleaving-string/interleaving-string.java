a23fe888c49bee6633a23072d67e9907ea1a5f59
nitinpcs <nitinpachauri937@gmail.com>
2026-03-20 22:31:48 +0530

    Time: 1 ms (91.43%) | Memory: 43.4 MB (52.71%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    Boolean[][] dp;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length() + s2.length() != s3.length()) return false;
+        dp = new Boolean[s1.length()+1][s2.length()+1];
+        return helper(0, 0, s1, s2, s3);
+    }
+
+    boolean helper(int i, int j, String s1, String s2, String s3){
+        int k = i + j;
+
+        if(i == s1.length() && j == s2.length()) return true;
+
+        if(dp[i][j] != null) return dp[i][j];
+        boolean ans = false;
+
+        if(i < s1.length() && s1.charAt(i) == s3.charAt(k)){
+            ans = ans || helper(i+1, j, s1, s2, s3);
+        }
+
+        if(j < s2.length() && s2.charAt(j) == s3.charAt(k)){
+            ans = ans || helper(i, j+1, s1, s2, s3);
+        }
+
+        return dp[i][j] = ans;
+    }
+}
