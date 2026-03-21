001b5f6db50e29178c65307b46444bd7a80c01ad
nitinpcs <nitinpachauri937@gmail.com>
2026-03-21 12:58:20 +0530

    Time: 3 ms (98.15%) | Memory: 47.4 MB (28.13%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,26 +1,26 @@
 class Solution {
+    int[][] dp;
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
+        dp = new int[n+1][m+1];
+        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
 
-        return helper(word1, word2, dp, n, m);
+        return helper(n, m, word1, word2);
     }
 
-    int helper(String word1, String word2, int[][] dp, int i, int j) {
-        if(i == 0) return j;
+    int helper(int i, int j, String word1, String word2) {
+        if(i == 0) return  j;
         if(j == 0) return i;
         if(dp[i][j] != -1) return dp[i][j];
-
         if(word1.charAt(i-1) == word2.charAt(j-1)) {
-            return dp[i][j] = helper(word1, word2, dp, i-1, j-1);
+            return dp[i][j] = helper(i-1, j-1, word1, word2);
         }
 
-        int n1 = helper(word1, word2, dp, i-1, j-1);
-        int n2 = helper(word1, word2, dp, i-1, j);
-        int n3 = helper(word1, word2, dp, i, j-1);
+        int deletion = helper(i-1, j, word1, word2);
+        int insertion = helper(i, j-1, word1, word2);
+        int replace = helper(i-1, j-1, word1, word2);
 
-        return dp[i][j] = Math.min(n1, Math.min(n2, n3)) + 1;
+        return dp[i][j] = 1 + Math.min(deletion, Math.min(insertion, replace)) ;
     }
 }
