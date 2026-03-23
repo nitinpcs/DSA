c9b53925e77c0c5db55bb9c2fd629d9c07813d83
nitinpcs <nitinpachauri937@gmail.com>
2026-03-23 22:25:52 +0530

    Time: 2 ms (56.93%) | Memory: 44.1 MB (70.79%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
@@ -7,8 +7,8 @@ class Solution {
         long[][] minDp = new long[n][m];
 
         maxDp[0][0] = minDp[0][0] = grid[0][0];
-        for(int i=1; i<n; i++) maxDp[i][0] = minDp[i][0] = maxDp[i-1][0]*grid[i][0];
-        for(int j=1; j<m; j++) maxDp[0][j] = minDp[0][j] = maxDp[0][j-1]*grid[0][j];
+        for(int i=1; i<n; i++) maxDp[i][0] = minDp[i][0] = minDp[i-1][0]*grid[i][0];
+        for(int j=1; j<m; j++) maxDp[0][j] = minDp[0][j] = minDp[0][j-1]*grid[0][j];
 
         for(int i=1; i<n; i++) {
             for(int j=1; j<m; j++) {
