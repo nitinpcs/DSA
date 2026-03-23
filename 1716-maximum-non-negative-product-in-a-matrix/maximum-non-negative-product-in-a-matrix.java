8df267f30a89aaef1b1c73a4772f1354224cb7c3
nitinpcs <nitinpachauri937@gmail.com>
2026-03-23 22:32:17 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (12.38%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
@@ -1,30 +1,50 @@
 class Solution {
     static final int MOD = 1000000007;
+    int n, m;
+    int[][] grid;
+    Long[][] maxMemo;
+    Long[][] minMemo;
+
     public int maxProductPath(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        long[][] maxDp = new long[n][m];
-        long[][] minDp = new long[n][m];
+        this.grid = grid;
+        n = grid.length;
+        m = grid[0].length;
+        maxMemo = new Long[n][m];
+        minMemo = new Long[n][m];
 
-        maxDp[0][0] = minDp[0][0] = grid[0][0];
-        for(int i=1; i<n; i++) maxDp[i][0] = minDp[i][0] = minDp[i-1][0]*grid[i][0];
-        for(int j=1; j<m; j++) maxDp[0][j] = minDp[0][j] = minDp[0][j-1]*grid[0][j];
+        long[] result = dfs(n - 1, m - 1);
+        long maxProduct = result[0];
 
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                long val = grid[i][j];
-                long maxTop = Math.max(maxDp[i-1][j]*val, minDp[i-1][j]*val);
-                long maxLeft = Math.max(maxDp[i][j-1]*val, minDp[i][j-1]*val);
-                
-                long minTop = Math.min(maxDp[i-1][j]*val, minDp[i-1][j]*val);
-                long minLeft = Math.min(maxDp[i][j-1]*val, minDp[i][j-1]*val);
+        return maxProduct < 0 ? -1 : (int)(maxProduct % MOD);
+    }
 
-                maxDp[i][j] = Math.max(maxLeft, maxTop);
-                minDp[i][j] = Math.min(minLeft, minTop);
-            }
-            
+    long[] dfs(int i, int j) {
+        if (i == 0 && j == 0) {
+            return new long[]{grid[0][0], grid[0][0]};
         }
-        long res = maxDp[n-1][m-1];  
-        return res < 0 ? -1 : (int)(res % MOD);
+
+        if (maxMemo[i][j] != null && minMemo[i][j] != null) {
+            return new long[]{maxMemo[i][j], minMemo[i][j]};
+        }
+
+        long val = grid[i][j];
+        long maxProd = Long.MIN_VALUE;
+        long minProd = Long.MAX_VALUE;
+
+        if (i > 0) {
+            long[] top = dfs(i - 1, j);
+            maxProd = Math.max(top[0] * val, top[1] * val);
+            minProd = Math.min(top[0] * val, top[1] * val);
+        }
+
+        if (j > 0) {
+            long[] left = dfs(i, j - 1);
+            maxProd = Math.max(maxProd, Math.max(left[0] * val, left[1] * val));
+            minProd = Math.min(minProd, Math.min(left[0] * val, left[1] * val));
+        }
+
+        maxMemo[i][j] = maxProd;
+        minMemo[i][j] = minProd;
+        return new long[]{maxProd, minProd};
     }
 }
