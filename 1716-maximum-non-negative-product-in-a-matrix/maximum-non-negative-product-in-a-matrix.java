0609b627ca747d1947f7de1973a84446b957bc71
nitinpcs <nitinpachauri937@gmail.com>
2026-03-23 22:34:17 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (12.38%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
@@ -12,15 +12,15 @@ class Solution {
         maxMemo = new Long[n][m];
         minMemo = new Long[n][m];
 
-        long[] result = dfs(n - 1, m - 1);
+        long[] result = dfs(0, 0);
         long maxProduct = result[0];
 
         return maxProduct < 0 ? -1 : (int)(maxProduct % MOD);
     }
 
     long[] dfs(int i, int j) {
-        if (i == 0 && j == 0) {
-            return new long[]{grid[0][0], grid[0][0]};
+        if (i == n - 1 && j == m - 1) {
+            return new long[]{grid[i][j], grid[i][j]};
         }
 
         if (maxMemo[i][j] != null && minMemo[i][j] != null) {
@@ -31,16 +31,16 @@ class Solution {
         long maxProd = Long.MIN_VALUE;
         long minProd = Long.MAX_VALUE;
 
-        if (i > 0) {
-            long[] top = dfs(i - 1, j);
-            maxProd = Math.max(top[0] * val, top[1] * val);
-            minProd = Math.min(top[0] * val, top[1] * val);
+        if (i + 1 < n) {
+            long[] down = dfs(i + 1, j);
+            maxProd = Math.max(maxProd, Math.max(down[0] * val, down[1] * val));
+            minProd = Math.min(minProd, Math.min(down[0] * val, down[1] * val));
         }
-
-        if (j > 0) {
-            long[] left = dfs(i, j - 1);
-            maxProd = Math.max(maxProd, Math.max(left[0] * val, left[1] * val));
-            minProd = Math.min(minProd, Math.min(left[0] * val, left[1] * val));
+        
+        if (j + 1 < m) {
+            long[] right = dfs(i, j + 1);
+            maxProd = Math.max(maxProd, Math.max(right[0] * val, right[1] * val));
+            minProd = Math.min(minProd, Math.min(right[0] * val, right[1] * val));
         }
 
         maxMemo[i][j] = maxProd;
