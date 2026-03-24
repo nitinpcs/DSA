6907bba61b594d8ac561ebb144c9080f403d5113
nitinpcs <nitinpachauri937@gmail.com>
2026-03-24 12:01:27 +0530

    Time: 7 ms (79.33%) | Memory: 45 MB (91.02%) - LeetSync

---
+++ 87-scramble-string/scramble-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        if (n != s2.length()) return false;
+
+        boolean[][][] dp = new boolean[n][n][n + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j][1] = (s1.charAt(i) == s2.charAt(j));
+            }
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                for (int j = 0; j + len <= n; j++) {
+
+                    for (int k = 1; k < len; k++) {
+
+                        if (dp[i][j][k] && dp[i + k][j + k][len - k]) {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+
+                        if (dp[i][j + len - k][k] && dp[i + k][j][len - k]) {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][n];
+    }
+}
