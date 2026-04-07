57b32eb1ce73d8f55a22b8387772d24da62402e7
nitinpcs <nitinpachauri937@gmail.com>
2026-04-08 00:32:53 +0530

    Time: 71 ms (59.44%) | Memory: 58 MB (55.24%) - LeetSync

---
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -0,0 +1,67 @@
+class Robot {
+    int w, h;
+    int x = 0, y = 0;
+    int dir = 1; // 0=N,1=E,2=S,3=W
+    int perimeter;
+
+    public Robot(int width, int height) {
+        this.w = width;
+        this.h = height;
+        this.perimeter = 2 * (w + h) - 4;
+    }
+    
+    public void step(int num) {
+        if (perimeter == 0) return;
+
+        num %= perimeter;
+
+        if (num == 0) {
+            if (x == 0 && y == 0) {
+                dir = 2; 
+            }
+            return;
+        }
+
+        while (num-- > 0) {
+            if (dir == 1) { 
+                if (x + 1 < w) x++;
+                else {
+                    dir = 0;
+                    y++;
+                }
+            } 
+            else if (dir == 0) { 
+                if (y + 1 < h) y++;
+                else {
+                    dir = 3;
+                    x--;
+                }
+            } 
+            else if (dir == 3) { 
+                if (x - 1 >= 0) x--;
+                else {
+                    dir = 2;
+                    y--;
+                }
+            } 
+            else {
+                if (y - 1 >= 0) y--;
+                else {
+                    dir = 1;
+                    x++;
+                }
+            }
+        }
+    }
+    
+    public int[] getPos() {
+        return new int[]{x, y};
+    }
+    
+    public String getDir() {
+        if (dir == 0) return "North";
+        if (dir == 1) return "East";
+        if (dir == 2) return "South";
+        return "West";
+    }
+}
