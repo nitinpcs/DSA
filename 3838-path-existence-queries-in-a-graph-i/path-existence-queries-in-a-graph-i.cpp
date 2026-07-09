f5098a3360b38e53fba3aba2b955ebc057969c7a
nitinpcs <nitinpachauri937@gmail.com>
2026-07-09 13:53:10 +0530

    Time: 22 ms (54.25%) | Memory: 223.9 MB (90.41%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int> component (n);
-        int val = 0;
-        component[0] = 0;
-        for(int i = 1; i < n; i++) {
-            if(nums[i] - nums[i-1] > maxDiff) {
-                val++;
+        vector<int> parent(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+
+        for(int i = 1; i < n ; i++) {
+            if(nums[i] - nums[i-1] <= maxDiff) {
+                unionSet(parent, i, i-1);
             }
-            component[i] = val;
         }
 
         vector<bool> ans(queries.size());
         int i = 0;
         for(const auto& query : queries) {
-            int u = query[0];
-            int v = query[1];
-            ans[i++] = component[u] == component[v]; 
+            int x = query[0];
+            int y = query[1];
+            if(find(parent, x) == find(parent, y)) ans[i++] = true;
+            else ans[i++] = false;
         }
 
         return ans;
+    }
+
+    int find(vector<int> &parent, int x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent, parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unionSet(vector<int> &parent , int x, int y) {
+        parent[find(parent, x)] = find(parent, y);
     }
 };
