1084d58111a280430b4157174be1ec289b23f3b2
nitinpcs <nitinpachauri937@gmail.com>
2026-07-20 10:48:36 +0530

    Time: 1 ms (62.53%) | Memory: 43.6 MB (7.93%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,68 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Trie t = new Trie();
+        for(String s : strs) t.insert(s);
+
+        return t.lcp(t.root);
+    }
+}
+
+
+class Trie {
+    Node root ;
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(String word) {
+        Node node = root;
+        for(char ch : word.toCharArray()) {
+            if(!node.containsKey(ch)) {
+                node.put(ch, new Node());
+            }
+            node = node.get(ch);
+        }
+        node.setEnd();
+    }
+
+    String lcp(Node node) {
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            int count = 0;
+            int index = -1;
+            for(int i=0; i<26; i++) {
+                if(node.child[i] != null) {
+                    count++;
+                    index = i;
+                }
+            }
+            if(count != 1 || node.isEnd()) break;
+            sb.append((char)(index+'a'));
+            node = node.get((char)(index+'a'));
+        }
+        return sb.toString();
+    }
+}
+
+class Node {
+    Node[] child = new Node[26];
+    boolean ew = false;
+
+    void put(char ch, Node node) {
+        child[ch-'a'] = node;
+    }
+    boolean containsKey(char ch) {
+        return child[ch-'a'] != null;
+    }
+    Node get(char ch) {
+        return child[ch-'a'];
+    }
+    boolean isEnd() {
+        return ew;
+    }
+    void setEnd() {
+        ew = true;
+    }
+}
+
+
