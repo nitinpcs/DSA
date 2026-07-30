04a06b3539fb73d53c802031b976739116876724
nitinpcs <nitinpachauri937@gmail.com>
2026-07-30 14:46:20 +0530

    Time: 9 ms (94.48%) | Memory: 48.4 MB (17.44%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -3,22 +3,20 @@ class Solution {
         int n = word.length();
         int[] freq = new int[26];
         for(char ch : word.toCharArray()) freq[ch-'a']++;
-        Integer[] idx = new Integer[26];
-        for(int i=0; i<26; i++) idx[i] = i;
-        Arrays.sort(idx, (a,b) -> freq[b] - freq[a]);
+        Arrays.sort(freq);
 
         int counter = 1;
         int push = 0;
-        int i=0;
-        while(i < 26) {
+        int i=25;
+        while(i >= 0) {
             int j=i;
             int temp = 0;
-            while(j < 26 && temp < 8) {
-                if(freq[idx[j]] > 0) {
-                    push += freq[idx[j]]*counter;
+            while(j >= 0 && temp < 8) {
+                if(freq[j] > 0) {
+                    push += freq[j]*counter;
                     temp++;
                 }
-                j++;
+                j--;
             }
             i = j;
             counter++;
