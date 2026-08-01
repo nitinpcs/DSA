7fac911af8f0b598f02aed5aa2dc8ba4a6f0525f
nitinpcs <nitinpachauri937@gmail.com>
2026-08-02 00:42:09 +0530

    Time: 67 ms (83.48%) | Memory: 46 MB (90.46%) - LeetSync

---
+++ 464-can-i-win/can-i-win.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        int mask = (1 << maxChoosableInteger) - 1;
+        int sum = (maxChoosableInteger*(maxChoosableInteger+1))/2;
+        if(sum < desiredTotal) return false;
+        if(desiredTotal < 0) return true;
+        Boolean[] dp = new Boolean[1  << maxChoosableInteger];
+        return whowins(maxChoosableInteger, desiredTotal, 0, mask, dp,true);
+    }
+
+    boolean whowins(int max, int total, int cur, int mask, Boolean[] dp, boolean first) {
+        if(dp[mask] != null) return dp[mask];
+
+        for(int i=0; i<max; i++) {
+            if((mask & (1<<i)) == 0) continue;
+            if(cur + i+1 >= total) {
+                return dp[mask] = true;
+            }
+            if(!whowins(max, total, cur+i+1, mask^(1<<i), dp, !first)) return dp[mask] = true;
+        }
+        return dp[mask] = false;
+    }
+}
