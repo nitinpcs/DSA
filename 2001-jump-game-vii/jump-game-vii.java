6ddec5d66ca5fc0c0062c82592c07269e8c001fb
nitinpcs <nitinpachauri937@gmail.com>
2026-08-02 14:45:12 +0530

    Time: 10 ms (61.36%) | Memory: 47.7 MB (77.72%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.java
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
-        int n = s.length();
-        int[] f = new int[n];
+        int n =s.length();
+        int[] reach = new int[n];
+        reach[0] = 1;
         int[] pre = new int[n];
-        f[0] = 1;
-        for (int i = 0; i < minJump; i++) {
+        for(int i=0; i<minJump; i++) {
             pre[i] = 1;
         }
-        for (int i = minJump; i < n; i++) {
-            int left = i - maxJump;
-            int right = i - minJump;
-            if (s.charAt(i) == '0') {
-                int total = pre[right] - (left <= 0 ? 0 : pre[left - 1]);
-                f[i] = total != 0 ? 1 : 0;
+
+        for(int i=minJump; i<n; i++) {
+            int left = i-maxJump;
+            int right = i-minJump;
+            if(s.charAt(i) == '0') {
+                int totalreachable = pre[right] - (left<=0?0:pre[left-1]);
+                if(totalreachable > 0) reach[i] = 1;
             }
-            pre[i] = pre[i - 1] + f[i];
+            pre[i] = pre[i-1] + reach[i];
         }
-        return f[n - 1] == 1;
+
+        return reach[n-1] == 1;
     }
 }
