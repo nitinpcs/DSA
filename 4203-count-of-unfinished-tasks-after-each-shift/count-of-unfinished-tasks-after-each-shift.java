52f2a31bf31bdaf61a96a2430e6bd4694e1046d7
nitinpcs <nitinpachauri937@gmail.com>
2026-08-04 21:38:35 +0530

    Time: 23 ms (9.24%) | Memory: 163.6 MB (9.79%) - LeetSync

--- 4203-count-of-unfinished-tasks-after-each-shift/count-of-unfinished-tasks-after-each-shift.java
+++ 4203-count-of-unfinished-tasks-after-each-shift/count-of-unfinished-tasks-after-each-shift.java
@@ -4,33 +4,32 @@ class Solution {
         long[] p = new long[n];
         p[0] = tasks[0];
         for(int i=1; i<n; i++) p[i] = p[i-1] + tasks[i];
-
-        int m = shifts.length;
-        int[] ans = new int[m];
         long work = 0;
-        for(int i=0; i<m; i++) {
+        int[] ans = new int[shifts.length];
+        for(int i=0; i<shifts.length; i++) {
             work += shifts[i];
             if(work >= p[n-1]) {
                 work = 0;
                 continue;
             }
-            int last = bs(p, work);
-            ans[i] = n - (last + 1);
+            int completed = bs(p, work);
+            ans[i] = n - completed;
         }
         return ans ;
     }
 
     int bs(long[] p, long t) {
-        int s = 0, e = p.length-1;
+        int s = 0;
+        int e = p.length - 1;
         int ans = -1;
         while(s <= e) {
             int m = s + (e-s)/2;
-            if(p[m] <= t) {
+            if(p[m] > t) {
                 ans = m;
-                s = m+1;
+                e = m-1;
             }
-            else e=m-1;
+            else s = m+1;
         }
-        return ans;
+        return ans ;
     }
 }
