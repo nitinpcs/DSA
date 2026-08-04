0753da9837f991d0e1384e293eafce83b3db8808
nitinpcs <nitinpachauri937@gmail.com>
2026-08-04 21:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 125.3 MB (67.31%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -2,11 +2,10 @@ class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
         if(n <= 2) return n;
-        int msb = 0;
-        while(n > 0) {
-            n = n >> 1;
-            msb++;
+        int ans = 2;
+        while(ans <= n) {
+            ans <<= 1;
         }
-        return 1 << msb;
+        return ans;
     }
 }
