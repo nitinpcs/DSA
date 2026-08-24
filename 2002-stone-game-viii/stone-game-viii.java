5cd74f7e3572f580819cc4406a8ef4bd9690ba05
nitinpcs <nitinpachauri937@gmail.com>
2026-08-24 12:24:17 +0530

    Time: 38 ms (6.38%) | Memory: 108.2 MB (5.11%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -2,12 +2,18 @@ class Solution {
     public int stoneGameVIII(int[] stones) {
         int n = stones.length;
         for(int i=1; i<n; i++) stones[i] += stones[i-1];
-        int[] dp = new int[n];
-        dp[n-1] = stones[n-1];
-        for(int i=n-2; i>=1; i--) {
-            dp[i] = Math.max(dp[i+1], stones[i] - dp[i+1]);
-        }
+        Integer[] dp = new Integer[n];
+        
+        return getDiff(1, stones, dp);
+    }
 
-        return dp[1];
+    int getDiff(int i, int[] stones, Integer[] dp) {
+        if(i == stones.length-1) return stones[stones.length - 1];
+        if(dp[i] != null) return dp[i];
+        
+        int pick = stones[i] - getDiff(i+1, stones, dp);
+        int notpick = getDiff(i+1, stones, dp);
+
+        return dp[i] = Math.max(pick, notpick);
     }
 }
