7cf2565bea0b8bcf910697a31858c5f834fb0993
nitinpcs <nitinpachauri937@gmail.com>
2026-09-08 16:05:51 +0530

    Time: 1067 ms (62.13%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT d.name As Department, e.name AS Employee, e.salary 
-FROM Employee e JOIN Department d ON e.departmentId = d.id
+SELECT Department , Employee, Salary FROM
 
-WHERE 3 > (
-    SELECT COUNT(DISTINCT e1.Salary) FROM Employee e1 
-    WHERE e1.departmentId = e.departmentId 
-    AND e1.Salary > e.Salary
-);
+(
+    SELECT d.name As Department, e.name AS Employee, e.Salary ,
+    DENSE_RANK() OVER (
+        PARTITION BY e.departmentId 
+        ORDER BY e.Salary DESC
+    ) AS rnk
+    FROM Employee e JOIN Department d ON e.departmentId = d.id
+) t
+
+WHERE rnk <= 3 ;
+
