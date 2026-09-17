c0f008d6f07ae188907dd9e25ad8b4b840fb29b1
nitinpcs <nitinpachauri937@gmail.com>
2026-09-17 12:49:41 +0530

    Time: 11 ms (49.78%) | Memory: 88.3 MB (93.72%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -10,25 +10,26 @@ class Solution {
 
         int l = 0;
         int curr = arr[0];
+        prefix[0] = curr == target ? 1 : INF;
         for(int r = 1; r < n; r++) {
+            curr += arr[r];
             while(curr > target) curr -= arr[l++];
             if(curr == target) {
-                prefix[r] = Math.min(prefix[r - 1], r - l);
+                prefix[r] = Math.min(prefix[r - 1], r - l + 1);
             }
             else prefix[r] = prefix[r-1];
-            curr += arr[r];
+           
         }
 
         curr = arr[n - 1];
-        suffix[n - 1] = curr == target ? 1 : INF;
         l = n - 1;
         for(int r = n - 2; r >= 0; r--) {
-            curr += arr[r];
             while(curr > target) curr -= arr[l--];
             if(curr == target) {
-                suffix[r] = Math.min(suffix[r+1], l - r + 1);
+                suffix[r] = Math.min(suffix[r+1], l - r);
             }
             else suffix[r] = suffix[r + 1];
+            curr += arr[r];
         }
 
         int minLength = INF;
