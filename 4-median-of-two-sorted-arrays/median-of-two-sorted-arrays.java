51c564c264569b4b51bb4628b78622c9b8145017
nitinpcs <nitinpachauri937@gmail.com>
2026-09-19 20:16:33 +0530

    Time: 1 ms (100.00%) | Memory: 48.6 MB (93.41%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,23 +1,35 @@
 class Solution {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-         int i=0,j=0,k=0;
-         int[] c=new int[a.length+b.length];
-        while(i<a.length && j<b.length){
-            if(a[i]<=b[j]){
-                c[k++]=a[i++];
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
+        int n = nums1.length;
+        int m = nums2.length;
+        int leftPart = (n + m + 1)/2;
+        int start = 0;
+        int end = n;
+        int MIN = Integer.MIN_VALUE;
+        int MAX = Integer.MAX_VALUE;
+
+        while(start <= end) {
+            int partA = start + (end - start) / 2;
+            int partB = leftPart - partA;
+
+            int lA = partA == 0 ? MIN : nums1[partA - 1];
+            int rA = partA == n ? MAX : nums1[partA];
+
+            int lB = partB == 0 ? MIN : nums2[partB - 1];
+            int rB = partB == m ? MAX : nums2[partB];
+
+            if(lA <= rB && lB <= rA) {
+                return (n + m) % 2 == 1 ? Math.max(lA, lB) : (Math.max(lA, lB) + Math.min(rA, rB)) / 2.0;
             }
-            else{
-                c[k++]=b[j++];
+            else if(lA > rB) {
+                end = partA - 1;
+            }
+            else {
+                start = partA + 1;
             }
         }
-        while(i<a.length){
-            c[k++]=a[i++];
-        }
-        while(j<b.length){
-            c[k++]=b[j++];
-        }
-        if(c.length%2==1)
-        return (double)c[c.length/2];
-        return (c[c.length/2-1]+c[c.length/2])/2.0;
+        return 0.0;
     }
 }
